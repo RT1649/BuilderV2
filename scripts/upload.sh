03798d122a7e6176645915e75fb21fefbba67e53
RT1649 <147801718+RT1649@users.noreply.github.com>
2024-01-28 23:24:21 +0530

    Update upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -21,7 +21,7 @@ uccache
 
 rom_upload() {
 cd ~/
-cd out/target/product/* && curl --upload-file ./crDroidAndroid-13.0-20240128-fleur-v9.13.zip https://transfer.sh/crDroidAndroid-13.0-20240128-fleur-v9.13.zip && curl --upload-file ./boot.img https://transfer.sh/boot.img
+cd out/target/product/* && curl --upload-file ./crDroidAndroid-13.0-20240129-fleur-v9.13.zip https://transfer.sh/crDroidAndroid-13.0-20240129-fleur-v9.13.zip && curl --upload-file ./boot.img https://transfer.sh/boot.img
 }
 echo "Uploading Rom & Boot From /Out"
 rom_upload
