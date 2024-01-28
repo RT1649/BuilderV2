0efc5567bc0d91042dffa209171f56e4bb86ac35
RT1649 <147801718+RT1649@users.noreply.github.com>
2024-01-28 23:13:58 +0530

    Disable ccache upload for now and update upload parameters

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -13,7 +13,7 @@ echo "Compressing New CCACHE"
 time compress ccache 1
 
 uccache() {
-curl --upload-file ./ccache.tar.gz https://transfer.sh/ccache.tar.gz
+#curl --upload-file ./ccache.tar.gz https://transfer.sh/ccache.tar.gz
 
 }
 echo "Uploading CCACHE"
@@ -21,7 +21,7 @@ uccache
 
 rom_upload() {
 cd ~/
-cd out/target/product/* && curl --upload-file ./crDroidAndroid-13.0-20240124-fleur-v9.13.zip https://transfer.sh/crDroidAndroid-13.0-20240124-fleur-v9.13.zip && curl --upload-file ./boot.img https://transfer.sh/boot.img
+cd out/target/product/* && curl --upload-file ./crDroidAndroid-13.0-20240128-fleur-v9.13.zip https://transfer.sh/crDroidAndroid-13.0-20240128-fleur-v9.13.zip && curl --upload-file ./boot.img https://transfer.sh/boot.img
 }
 echo "Uploading Rom & Boot From /Out"
 rom_upload
